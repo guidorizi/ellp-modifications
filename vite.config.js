fd7821eb99982fe746e418aae812c2cbb407f3de
João Victor Guidorizi <127569199+guidorizi@users.noreply.github.com>
2025-07-01 00:31:46 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-base: '/ellp-modifications/' // Corrigido para URL absoluto})
+ base: '/ellp-modifications/'
+})
