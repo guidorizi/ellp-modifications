22407716fe0dfda09c7d13e03f918139485c4398
João Victor Guidorizi <127569199+guidorizi@users.noreply.github.com>
2025-07-01 08:56:59 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Home from "./pages/Home";
 import Eventos from "./pages/EventosSection";
 import EventosEspecificos from "./pages/Eventos_Especificos";
 import Voluntarios from "./pages/VoluntariosSection";
+import VoluntariosResumo from "./pages/VoluntariosResumo";
 
 function App() {
   return (
-    <Router basename="/ellp-modifications/">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/eventos" element={<Eventos />} />
         <Route path="/eventos/:id" element={<EventosEspecificos />} />
         <Route path="/voluntarios" element={<Voluntarios />} />
+        <Route path="/voluntariosresumo" element={<VoluntariosResumo />} />
       </Routes>
     </Router>
   );
